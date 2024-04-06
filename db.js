015290f646bee8b81c5e110edb7867d6949ac2a2
Durgesh1438 <durgesh.kovvuri1438@gmail.com>
2024-04-06 10:58:31 +0530

    changes made

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 const mysql = require('mysql');
 
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'blogdb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 
